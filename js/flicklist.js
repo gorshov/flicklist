eb1ec7d8464e81d738774061d3ca6c2afc930490
gorshov <strf@tut.by>
2020-05-10 19:50:04 +0300

    step 0

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,8 +1,6 @@
-
-
 var api = {
-	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
+    root: "https://api.themoviedb.org/3",
+    token: "79cac75ea4961a258546cd642c279087" // TODO put your api key here
 }
 
 /**
@@ -10,16 +8,19 @@ var api = {
  * if successful, prints the results to the console
  */
 function testTheAPI() {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		}
-	});
+    $.ajax({
+        url: api.root + "/discover/movie",
+        data: {
+            api_key: api.token,
+        },
+        success: function (response) {
+            console.log("We got a response from The Movie DB!");
+            console.log(response);
+        },
+        error: () => {
+			console.log("An error occurred while connection to api.")
+        }
+    });
 }
 
 
